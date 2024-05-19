503fcbcf42e7c4cbea9b3195ed92b225c7e089fd
aburke76 <alexlburke8771@gmail.com>
2024-05-19 13:47:37 -0400

    arrow buttons work, will cycle through all images

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 const menuIcon = document.querySelector("#menu-icon");
 const dots = document.querySelectorAll(".dot");
-const picInFrame = document.querySelector(".show");
 const pictureFrame = document.querySelector(".picture-frame");
 const nextBtn = document.querySelector(".right-arrow");
 const prevBtn = document.querySelector(".left-arrow");
 const images = document.querySelectorAll("img");
+const slides = document.querySelector(".slides");
 
 menuIcon.addEventListener("click", () => {
     toggleMenu();
 });
 
 nextBtn.addEventListener("click", () => {
-    images.forEach((img, index) => {
+    const shownImg = imgInFrame();
+    shownImg.classList.toggle("hidden");
+    if (shownImg.id == 7) {
+        images[0].classList.toggle("hidden");
+    } else {
+        const next = parseInt(shownImg.id) + 1;
+        images[next - 1].classList.toggle("hidden");
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    const shownImg = imgInFrame();
+    shownImg.classList.toggle("hidden");
+    if (shownImg.id == 1) {
+        images[images.length - 1].classList.toggle("hidden");
+    } else {
+        const prev = parseInt(shownImg.id) - 1;
+        images[prev - 1].classList.toggle("hidden");
+    }
+});
+
+function imgInFrame() {
+    let shownImg;
+    images.forEach((img) => {
         if (!img.classList.contains("hidden")) {
-            img.classList.toggle("hidden");
-            // console.log(images[index + 1].classList);
-            images[index + 1].classList.toggle("hidden");
+            shownImg = img;
         }
     });
-    //     for (let i = 0; i < images.length; i++) {
-    //         if (!images[i].classList.contains("hidden")) {
-    //             images[i].classList.toggle("hidden");
-    //             pictureFrame.remove(images[i]);
-    //             console.log(images[i + 1].classList);
-    //             images[i + 1].classList.toggle("hidden");
-    //         }
-    //     }
-});
+    return shownImg;
+}
 
 function toggleMenu() {
     if (dropdown.classList.value.includes("up")) {
